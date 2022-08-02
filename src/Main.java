87e4fc953cfc5f9e7355d6ad3dadad2f7f5ab243
VMishutkin <vladmish@list.ru>
2022-08-02 15:16:23 +0700

    Выполнил ДЗ

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,33 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        //Задача 1
+        int i = 1;
+        while (i<=10){
+            System.out.print(i + " ");
+            i++;
+        }
+        i=10;
+        System.out.println();
+        while (i>0){
+            System.out.print(i + " ");
+            i--;
+        }
+        System.out.println();
+        // Задача 2
+        int firstFriday = 1;
+        for(int day = firstFriday; day<=31 ; day+=7){
+            System.out.println("Сегодня пятница " + day + "-е число, Необходимо приготовить отчет");
+        }
+        System.out.println();
+        //Задача 3
+        int thisYear = 2022;
+        for(int cometYearCounter = 0; cometYearCounter <= thisYear+100; cometYearCounter +=79){
+            if (cometYearCounter>=thisYear -200 && cometYearCounter<= thisYear + 100){
+                System.out.println(cometYearCounter);
+            }
+        }
+
+
+
     }
 }
